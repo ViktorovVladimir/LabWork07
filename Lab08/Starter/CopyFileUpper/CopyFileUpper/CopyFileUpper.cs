19d64a715aae5ac9c199690945dab3bf14f83f91
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-05-29 12:42:44 +0300

    ex.3 was complete
    - выполнил упражнение №3 "Создание файла с текстом измененного формата"

---
+++ Lab08/Starter/CopyFileUpper/CopyFileUpper/CopyFileUpper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace CopyFileUpperNameSpace
+{
+    class CopyFileUpper
+    {
+        //--.
+        static void Main()
+        {
+            string sFrom, sTo;
+            StreamReader srFrom;
+            StreamWriter swTo;
+
+            //--. Promt for input file name
+            Console.Write("Copy from: ");
+            sFrom = Console.ReadLine();
+
+            //--. Promt for output file name
+            Console.Write("Copy to: ");
+            sTo = Console.ReadLine();
+
+            //--.
+            Console.WriteLine("Copy from {0} to {1}", sFrom, sTo);
+
+            //--.
+            try
+            {
+                //--.
+                srFrom = new StreamReader(sFrom);
+                swTo = new StreamWriter(sTo);
+
+                //--.
+                while (srFrom.Peek() != -1)
+                {
+                    string sBuffer = srFrom.ReadLine();
+                    sBuffer = sBuffer.ToUpper();
+                    swTo.WriteLine(sBuffer);
+                }
+
+                //--.
+                swTo.Close();
+                srFrom.Close();
+            }
+            catch (FileNotFoundException)
+            {
+                //--.
+                Console.WriteLine("Input file not found");
+            }
+            catch (Exception e)
+            {
+                //--.
+                Console.WriteLine("Unexpected exception");
+                Console.WriteLine(e.ToString() );
+            }
+        }
+    }
+}
