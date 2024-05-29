087c691d17ad63e8d735f1664dab27d29284ccb9
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-05-30 00:13:25 +0300

    add ex.4 was complete
    - Выполнит упражнение №4 "Проверка наличия реализации интерфейса"

---
+++ Lab08/Starter/InterfaceTest/InterfaceTest/Test.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace InterfaceTestNameSpace
+{
+    using System;
+
+    //--.
+    public class Test
+    {
+        public static void Main()
+        {
+            
+            int i = 0;
+            ulong ul = 0;
+            string s = "Test";
+
+            Console.WriteLine( "int: {0}", Utils.IsItFormattable(i)    );
+            Console.WriteLine( "ulong: {0}", Utils.IsItFormattable(ul) );
+            Console.WriteLine("string: {0}", Utils.IsItFormattable(s))   ;
+        }
+    }
+}
---
+++ Lab08/Starter/InterfaceTest/InterfaceTest/Utils.cs
@@ -0,0 +1,38 @@
+﻿
+
+
+namespace InterfaceTestNameSpace
+{
+    using System;
+
+    //--.
+    class Utils
+    {
+        
+        //--.
+        public static void Reverse(ref string s)
+        {
+            string sRev = "";
+
+            //--.
+            for(int k=s.Length-1; k >= 0; k--)
+            {
+                sRev = sRev + s[k];
+            }
+
+            //--. Return result to caller
+            s = sRev;
+        }
+
+
+        //--.
+        public static bool IsItFormattable(object x)
+        {   
+            if(x is IFormattable)
+                return true;
+            else
+                return false;
+        }   
+
+    }
+}
