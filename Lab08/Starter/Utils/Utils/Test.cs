a5228f7703ab98ce01308207f2e5be1e5c563c3a
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-05-29 12:10:07 +0300

    ex.2 reverse string was complete
    - сделал упражнение №2 "Обращение строки"

---
+++ Lab08/Starter/Utils/Utils/Test.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace UtilsLabWork07NameSpaceL
+{
+    using System;
+
+    //--.
+    public class Test
+    {
+        public static void Main()
+        {
+            string message;
+            
+
+            //while(true)
+            //{
+
+
+
+            //--. Get an input string
+            Console.WriteLine("Enter string to reverse: ");
+            message = Console.ReadLine();
+
+            //--. Reverse the string
+            Utils.Reverse(ref message);
+
+            //--. Display the result
+            Console.WriteLine(message);
+
+
+
+            //}
+
+
+        }
+    }
+}
---
+++ Lab08/Starter/Utils/Utils/Utils.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+namespace UtilsLabWork07NameSpaceL
+{
+    using System;
+
+    //--.
+    class Utils
+    {
+        
+        //--.
+        public static void Reverse(ref string s)
+        {
+            string sRev = "";
+
+            //--.
+            for(int k=s.Length-1; k >= 0; k--)
+            {
+                sRev = sRev + s[k];
+            }
+
+            //--. Return result to caller
+            s = sRev;
+        }
+    }
+}
