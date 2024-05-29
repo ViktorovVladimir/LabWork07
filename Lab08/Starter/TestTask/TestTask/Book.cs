babf637594d488622c30b10cb2ee019d7fbba672
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-05-30 01:54:51 +0300

    ex.6 test task was complete
    - выполнит контрольное упражнение №6 "Реализация сортировка объектов класса"

---
+++ Lab08/Starter/TestTask/TestTask/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TestTaskNameSpace
+{
+    using System;
+    using System.Collections;
+
+    //--.
+    class Book : IComparable
+    {
+        String author, title;
+        int pages, year;
+
+        //--.
+        public Book()
+        {
+            author = "";
+            title = "";
+            pages = 0;
+            year = 0;
+        }
+
+        //--.
+        public void SetBook(String author, String title, int pages, int year)
+        {
+            this.author = author;
+            this.title = title;
+            this.pages = pages;
+            this.year = year;
+        }
+
+        //--.
+        public void Show()
+        {
+            Console.WriteLine("\nКнига:\n  Автор: {0}\n Название: {1}\n Год издания: {2}\n {3} стр.\n", author, title, year, pages);
+        }
+
+        public int CompareTo(object obj)
+        {
+            //--.
+            Book it = (Book)obj;
+            //--.
+            if(this.year == it.year)
+                return 0;
+            else if (this.year > it.year)
+                return 1;
+            else
+                return -1;
+        }
+    }
+
+}
---
+++ Lab08/Starter/TestTask/TestTask/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace TestTaskNameSpace
+{
+    using System;
+    using System.Collections;
+
+    //--.
+    public class Test
+    {
+        public static void Main()
+        {
+            ArrayList boooks = new ArrayList();
+
+            //--. Init random number generator
+            Random rndPages = new Random();
+            Random rndYear = new Random();
+
+            //--. Generate 10 books betwen with years: 1945 - 2024;
+            //--.                          with pages: 30 - 1000;
+
+
+            for(int i= 0; i<10; i++)
+            {
+                int pages = rndPages.Next(29, 1001);
+                int year = rndYear.Next(1944, 2025);
+                Book temp = new Book();
+                temp.SetBook( i.ToString(), (i + 1000).ToString(), pages, year);
+                boooks.Add(temp);
+            }
+
+            //--. Sort ArrayList
+            boooks.Sort();
+
+            //--.
+            foreach(Book temp in boooks)
+                temp.Show();
+
+
+        }
+    }
+
+}
