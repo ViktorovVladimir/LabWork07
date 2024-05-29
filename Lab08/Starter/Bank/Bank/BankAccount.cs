5277bf017b4da45c2026e9537f7c3040545382b6
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-05-29 10:53:07 +0300

    ex.1: add  an instance method with two parameters
    - добавление метода экземпляра с двумя параметрами

---
+++ Lab08/Starter/Bank/Bank/BankAccount.cs
@@ -0,0 +1,65 @@
+﻿
+using System.Security.Principal;
+
+class BankAccount
+{
+    long accNo;
+    decimal accBal;
+    AccountType accType;
+    //--.
+    static long nextAccNo = 123;
+
+    //--.
+    public void Populate( decimal balance )
+    {
+        this.accNo = NextNumber();
+        this.accBal = balance;
+        this.accType = AccountType.Checking;
+    }
+
+    //--.
+    public long Number()
+    {
+        return accNo;
+    }
+
+    //--.
+    public decimal Ballance()
+    {
+        return accBal;
+    }
+
+    //--.
+    public string Type()
+    {
+        return accType.ToString();
+    }
+
+    //--.
+    static long NextNumber()
+    {
+        return nextAccNo++;
+    }
+
+    //--.
+    public decimal Deposit(decimal amount ) 
+    {
+        accBal += amount;
+        return accBal;
+    }
+
+    //--.
+    public bool Withdraw(decimal amount) 
+    {
+        //--.
+        bool sufficientFunds = accBal >= amount;
+        //--.
+        if(sufficientFunds)
+        {
+            accBal -= amount;
+        }
+        //--.
+        return sufficientFunds;
+    }
+
+}
